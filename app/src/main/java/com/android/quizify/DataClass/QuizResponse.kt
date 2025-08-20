0bea964c32b851cc1bc0ee0fb22ba774f50ded8b
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-08-20 10:06:53 +0500

    data class added

--- app/src/main/java/com/android/quizify/DataClass/QuizResponse.kt
+++ app/src/main/java/com/android/quizify/DataClass/QuizResponse.kt
@@ -1,2 +1,18 @@
 package com.android.quizify.DataClass
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+@Parcelize
+data class QuizResponse(
+    val results: List<Question>
+): Parcelable
+
+
+@Parcelize
+data class Question(
+    val type: String,
+    val difficulty: String,
+    val category : String,
+    val question: String,
+    val correct_answer: String
+) : Parcelable
